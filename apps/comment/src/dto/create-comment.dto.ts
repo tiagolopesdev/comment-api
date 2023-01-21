44ccd36b35934e061da972db1292b7ea9909cf7a
Tiago Lopes <saxtiago14@gmail.com>
2023-01-21 18:58:43 -0300

    Feat - filter of data to return user

--- apps/comment/src/dto/create-comment.dto.ts
+++ apps/comment/src/dto/create-comment.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCommentDto {
+
+  id: string;
   
   @ApiProperty()
   title: string;
@@ -10,4 +12,14 @@ export class CreateCommentDto {
 
   @ApiProperty()
   author: string;
+
+  private constructor(id: string, body: string, author: string) {
+    this.id = id;
+    this.body = body;
+    this.author = author;
+  }
+
+  static convertAllElements (id: string, body: string, author: string) {
+    return new CreateCommentDto(id, body, author);
+  } 
 }
--- apps/comment/src/services/comment.service.ts
+++ apps/comment/src/services/comment.service.ts
@@ -9,18 +9,34 @@ export class CommentService {
 
   constructor(
     @InjectModel('comment') private readonly commentModel: Model<CommentDocument>
-  ){}
+  ) { }
 
   async getAllComments() {
-    try{
-      return await this.commentModel.find();
+    try {
+
+      const comments: CreateCommentDto[] = [];
+
+      const results = await this.commentModel.find();
+
+      results.map((item) => {
+        comments.push(
+          CreateCommentDto.convertAllElements(
+            item.id,
+            item.body,
+            item.author,
+          )
+        )
+      });
+
+      return comments;
+
     } catch (ex) {
       throw new InternalServerErrorException('Internal server error');
     }
   }
 
-  async createUser(createCommentDto: CreateCommentDto) : Promise<Comment>{    
-    try{
+  async createUser(createCommentDto: CreateCommentDto): Promise<Comment> {
+    try {
       const createComment = new this.commentModel(createCommentDto);
       const result = await createComment.save();
 
